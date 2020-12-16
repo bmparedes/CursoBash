d721f7511bddd61ce130b03bfb314eef3bb15f62
bmparedes <bmparedes5@gmail.com>
2020-12-16 19:14:34 -0400

    Carpeta de retos del curso, Reto numero 1

---
+++ Retos/utilityHost.sh
@@ -0,0 +1,10 @@
+# ! /bin/bash
+#Declarar dos variables una llamada option, otra llamda result y inicializarles e imprimir sus valores.
+#Autor: Brandom Paredes.
+
+option="Opcion"
+result=$1
+
+echo "Reto 1: Utility Host"
+echo "Valor de la variable option: $option"
+echo "Valor de la variable result: $result"
