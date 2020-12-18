ab0a2701c39717bf56e28fd07ed50a80dcae53c5
bmparedes <bmparedes5@gmail.com>
2020-12-17 21:33:50 -0400

    Descargar Informacion desde internet con wget

---
+++ 10_download.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la descarga de informacion desde internet utilizando el comando wget. 
+
+echo "Descargar informacion de internet"
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.zip
