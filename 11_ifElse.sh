e92d6b1507e6c6635b27503dbaf4fe0b4b3ae9b8
bmparedes <bmparedes5@gmail.com>
2020-12-21 19:43:29 -0400

    Condicionales - Sentencia Decisión If-else if - else

---
+++ 11_ifElse.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+# Programa para ejemplificar el uso de la setencia de decision if, else
+# Profesor: Marcos Toscano Freire - @martosfre
+
+notaClase=0
+edad=0
+
+echo "Ejemplo Sentencia if - else"
+read -n1 -p "Indique cual es su nota (1-9): " notaClase
+echo -e "\n"
+if (( $notaClase >= 7 )); then
+    echo "El alumno aprueba la materia"
+else
+    echo "El alumno reprueba la materia"
+fi
+
+read -p "Indique cual es su edad: " edad
+if [ $edad -le 18 ]; then
+    echo "La persona no puede sufragar"
+else
+    echo "La persona si puede sufragar"
+fi
