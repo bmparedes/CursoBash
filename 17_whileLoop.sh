56c54e9a368215d8821ec4c16e315c18bedbebbe
bmparedes <bmparedes5@gmail.com>
2021-01-03 00:50:31 -0400

    Sentencia While Loop

---
+++ 17_whileLoop.sh
@@ -0,0 +1,12 @@
+# ! /bin/bash
+# Programa para ejemplificar el uso de la sentencia de iteracion while
+# Profesor: Marco Toscano Freire - @martosfre
+
+numero=1
+
+while [ $numero -ne 10 ]
+do
+    echo "Imprimiendo $numero veces"
+    numero=$((numero + 1))
+done
+
