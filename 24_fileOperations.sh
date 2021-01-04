b31435f8fdfee2e711808479eb207121596d9272
bmparedes <bmparedes5@gmail.com>
2021-01-04 02:11:44 -0400

    Operaciones con archivos

---
+++ 24_fileOperations.sh
@@ -0,0 +1,16 @@
+# ! /bin/bash
+# Programa para ejemplificar las operaciones de un archivo. 
+# Profesor: Marco Toscano Freire - @martosfre
+
+echo "Operaciones de un archivo"
+mkdir -m 755 backupScripts
+
+echo -e "\nCopiar los scripts del directorio actual al nuevo directorio backupScripts"
+cp *.* backupScripts/
+ls -la backupScripts/
+
+echo -e "\nMover el directorio backupScripts a otra ubicacion: $HOME"
+mv backupScripts $HOME
+
+echo -e "\nEliminar los archivos .txt"
+rm *.txt
