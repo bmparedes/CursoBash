797fd205e6770cd8711522a1e2b52df50d17e79d
bmparedes <bmparedes5@gmail.com>
2021-01-07 03:19:22 -0400

    Como realizar el empaquetamiento de archivos con los comandos tar, gzip y pbzip2

--- 10_download.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-# Programa para ejemplificar el uso de la descarga de informacion desde internet utilizando el comando wget. 
-
-echo "Descargar informacion de internet"
-wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.zip
---
+++ 25_tar.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+# Programa para ejemplificar el empaquetamiento con el comando tar.
+# Profesor: Marco Toscano Freire - @martosfre
+
+echo "Empaquetar todos los scripts de la carpeta shellCourse"
+tar -cvf shellCourse.tar *.sh
